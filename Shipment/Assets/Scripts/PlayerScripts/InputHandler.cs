19249006487f3cf225d0d9e2ca9f452b02403704
Stickguy1 <adamkaci3@csu.fullerton.edu>
2022-09-21 10:53:37 -0700

    Isometric Camera and Movement Setup
    Took waaaaay to long to figure out how to do this, but than again, that's why we have YouTube. I'm working on adding a running thing if you press shift, can't be completely unoriginal can we?

---
+++ Shipment/Assets/Scripts/PlayerScripts/InputHandler.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public Vector2 InputVector { get; private set;}
+
+    public Vector3 MousePosition { get; private set; }
+
+    void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+        InputVector = new Vector2(h, v);
+
+        MousePosition = Input.mousePosition;
+    }
+}
